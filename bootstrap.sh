11c560dacffdcf5519ee2296c5006692db630f9b
AJ Yoo <ajarusan@arista.com>
2021-04-01 11:25:09 -0700

    add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+rsync --exclude ".git" \
+      --exclude ".DS_Store" \
+      --exclude "bootstrap.sh" \
+      --exclude "README.md" \
+      --exclude "Brewfile" \
+      -avh --no-perms . $HOME 
