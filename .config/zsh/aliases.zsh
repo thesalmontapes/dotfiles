57e45f8706b33d5aa7b08e4d9325e1826528a2e6
AJ Yoo <ajarusan@arista.com>
2022-10-13 10:40:00 -0700

    zsh/aliases: more git aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -6,10 +6,18 @@ alias gl="git log"
 alias gb="git branch"
 alias gs="git status"
 alias gc="git checkout"
+alias ga="git add"
+alias gd="git diff"
+alias gri="git rebase -i"
+alias gpr="git pull --rebase"
+alias gcm="git commit"
+alias gpsh="git push"
 alias gt="git log --all --graph --oneline --decorate"
 
 alias vim="nvim"
 
 alias pb="curl -F c=@- pb"
+
+alias kc="kubectl"
 
 alias refresh="source $HOME/.zshrc"
