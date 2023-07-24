185fd2c7e5cf5057db2f8b043051c6b2a8fba81a
AJ Yoo <ajarusan@arista.com>
2023-07-24 12:46:13 -0700

    tmux: rename tmux() to tmuxs()

--- .config/zsh/tmux.zsh
+++ .config/zsh/tmux.zsh
@@ -1,22 +1,16 @@
-tmux() {
+tmuxs() {
   local session_name="$1"
   
-  # Check if a session name is provided
-  if [ -z "$session_name" ]; then
-    # No session name provided, use default `tmux` behavior
-    command tmux
+  # Check if the session with the same name already exists
+  if tmux has-session -t "$session_name" 2>/dev/null; then
+    # Session already exists, attach to it
+    tmux attach-session -t "$session_name"
   else
-    # Check if the session with the same name already exists
-    if command tmux has-session -t "$session_name" 2>/dev/null; then
-      # Session already exists, attach to it
-      command tmux attach-session -t "$session_name"
-    else
-      # Session doesn't exist, create a new session with four windows
-      command tmux new-session -d -s "$session_name" -n "source" "zsh"
-      command tmux new-window -t "$session_name" -n "client" "zsh"
-      command tmux new-window -t "$session_name" -n "server" "zsh"
-      command tmux select-window -t "${session_name}:0"
-      command tmux attach-session -t "$session_name"
-    fi
+    # Session doesn't exist, create a new session with four windows
+    tmux new-session -d -s "$session_name" -n "source" "zsh"
+    tmux new-window -t "$session_name" -n "client" "zsh"
+    tmux new-window -t "$session_name" -n "server" "zsh"
+    tmux select-window -t "${session_name}:0"
+    tmux attach-session -t "$session_name"
   fi
 }
